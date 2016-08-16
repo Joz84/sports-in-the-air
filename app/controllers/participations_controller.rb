a6b386089a8c72ff5da9dc596425025dc0948f8d
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-08-16 18:54:25 +0200

    routes et controllers

---
+++ app/controllers/participations_controller.rb
@@ -0,0 +1,10 @@
+class ParticipationsController < ApplicationController
+  def destroy,
+  end
+
+  def create,
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  root to: "pages#home"
+  get to: "pages#dashboard"
+  #resources :sports, only: [:index, :show]
+  resources :events do
+    resources :messages, only: [:create]
+    #resources :places, only: [:create, :update]
+  end
+  #resources :users, only: [:show, :new, :create, :edit, :update, :destroy] do
+  resources :participations, only: [:destroy, :create, :index]
+    #resources :favorite_sports, only: [:create, :destroy, :update]
+  #end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Attachinary::Engine => "/attachinary"
-  root to: "pages#home"
 end
