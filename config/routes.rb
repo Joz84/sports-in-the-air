a175f7360e6ebfc823fdadb4f1a9cf64aa3467d4
Felix Bungener <felixbungener@gmail.com>
2016-08-17 14:15:21 +0200

    card style, a bit of event index view, routes for favorite sports

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :favorite_sports, only: [:index, :create, :destroy, :update]
+
   root to: "pages#home"
-  get to: "pages#dashboard"
+  get "/dashboard/", to: "pages#dashboard"
   #resources :sports, only: [:index, :show]
   resources :events do
     resources :messages, only: [:create]
@@ -10,9 +12,8 @@ Rails.application.routes.draw do
   end
   #resources :users, only: [:show, :new, :create, :edit, :update, :destroy] do
   resources :participations, only: [:destroy, :create, :index]
-    #resources :favorite_sports, only: [:create, :destroy, :update]
   #end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount Attachinary::Engine => "/attachinary"
+  # mount Attachinary::Engine => "/attachinary"
 end
