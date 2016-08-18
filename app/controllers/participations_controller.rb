a6be52237461d500525463e35fa6086e39167263
Felix Bungener <felixbungener@gmail.com>
2016-08-18 12:17:06 +0200

    participations controller create + events show

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,10 +1,24 @@
 class ParticipationsController < ApplicationController
-  def destroy,
-  end
-
-  def create,
-  end
-
   def index
   end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @participant = @event.participations.new(participations_params)
+
+    if @participant.save
+      redirect_to event_path(@event)
+    else
+      render "events/show"
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def participations_params
+      params.require(:participation).permit(:status, :review, :rating)
+    end
 end
