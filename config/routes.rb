194f26e17c5c412f16c76fe7aab3c5b257af40c9
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-08-19 09:58:16 +0200

    form of favorite sports on dashboard and other pretty corrections

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,16 @@ Rails.application.routes.draw do
 
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :favorite_sports, only: [:index, :create, :destroy, :update]
+  resources :favorite_sports, only: [:create, :destroy]
 
   root to: "pages#dashboard", constraints: lambda { |r| r.env["warden"].authenticate? }
   root to: "pages#home"
 
   get "/dashboard/", to: "pages#dashboard"
-  #resources :sports, only: [:index, :show]
   resources :events do
     resources :messages, only: [:create]
     resources :participations, only: [:destroy, :create]
-    #resources :places, only: [:create, :update]
   end
-  #resources :users, only: [:show, :new, :create, :edit, :update, :destroy] do
-  resources :participations, only: [:index]
-  #end
 
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
